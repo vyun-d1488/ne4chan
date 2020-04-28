6171260ec275e196ae9509a88f29bf30acefa05a
psinadog <ilyaspiypiy@gmail.com>
2020-04-28 17:34:23 +0600

    nodemon

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Koa = require("koa");
 const logger = require("koa-logger");
 const app = new Koa();
+const Port = process.env.Port || 80;
 
 app.use(logger());
 
 app.use((_this, next) => {
+
 	_this.body = "Hello world";
+
 });
 
-app.listen(3000);
+app.listen(Port);
